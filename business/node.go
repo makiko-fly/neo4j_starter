abc85471957ee2b6e019b5a82870a18f2eb35a41
chen.fei <chenfei@wallstreetcn.com>
2019-01-16 20:03:13 +0800

    return labels of node

--- business/node.go
+++ business/node.go
@@ -1,6 +1,6 @@
 package business
 
-var getDirectlyRelatedNodesStmt = "MATCH (a)-[]-(b) WHERE id(a) = $nodeId RETURN b"
+var getDirectlyRelatedNodesStmt = "MATCH (a)-[]-(b) WHERE id(a) = $nodeId RETURN b, labels(b)"
 
 func GetDirectlyRelatedNodes(nodeId int64) (interface{}, error) {
 	paramsMap := make(map[string]interface{})
