f5af119d317b6cd20e96318573e9f6440b5d0f98
chen.fei <chenfei@wallstreetcn.com>
2019-02-20 15:16:21 +0800

    add new HAS_CHILD

--- common/enum.go
+++ common/enum.go
@@ -4,9 +4,10 @@ type EnumRelation int
 
 const (
 	EnumRelation_PRODUCES       EnumRelation = 1001 // company -> product
-	EnumRelation_HAS_DOWNSTREAM              = 1002 // product -> product
-	EnumRelation_LISTED_AS                   = 1003 // company -> stock
-	EnumRelation_WITHIN_CHAIN                = 1004 // product -> chain
+	EnumRelation_HAS_DOWNSTREAM              = 2001 // product -> product
+	EnumRelation_HAS_CHILD                   = 2002 // product -> product
+	EnumRelation_LISTED_AS                   = 3001 // company -> stock
+	EnumRelation_WITHIN_CHAIN                = 4001 // product -> chain
 )
 
 var EnumRelationDefaultStr = "UNDEFINED"
@@ -17,6 +18,8 @@ func (self EnumRelation) String() string {
 		return "PRODUCES"
 	case EnumRelation_HAS_DOWNSTREAM:
 		return "HAS_DOWNSTREAM"
+	case EnumRelation_HAS_CHILD:
+		return "HAS_CHILD"
 	case EnumRelation_LISTED_AS:
 		return "LISTED_AS"
 	case EnumRelation_WITHIN_CHAIN:
